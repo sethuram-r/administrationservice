28594cba14ec4eced0815e26524a117ef96a2520
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2020-07-07 00:00:20 +0100

    completed liquibase integration

---
+++ src/main/resources/db/data/bucket_access_entity.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.bucket_access_entity (bucket_access_id, read, write) VALUES (1, false, false);
+INSERT INTO public.bucket_access_entity (bucket_access_id, read, write) VALUES (2, false, true);
+INSERT INTO public.bucket_access_entity (bucket_access_id, read, write) VALUES (3, true, false);
+INSERT INTO public.bucket_access_entity (bucket_access_id, read, write) VALUES (4, true, true);
+
---
+++ src/main/resources/db/data/object_access_entity.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO public.object_access_entity VALUES (1, false, false, false);
+INSERT INTO public.object_access_entity VALUES (2, true, false, false);
+INSERT INTO public.object_access_entity VALUES (3, false, false, true);
+INSERT INTO public.object_access_entity VALUES (4, true, false, true);
+INSERT INTO public.object_access_entity VALUES (5, false, true, false);
+INSERT INTO public.object_access_entity VALUES (6, true, true, false);
+INSERT INTO public.object_access_entity VALUES (7, false, true, true);
+INSERT INTO public.object_access_entity VALUES (8, true, true, true);
